bc5da77a151f9cba5668f82320ddbb1b0627b613
Alex Ehrenhöfer <ehren.alex@web.de>
2024-11-12 21:18:14 +0100

    added PC Tester Notebook

--- pipeline/components/pc_generator/DepthEstimator.py
+++ pipeline/components/pc_generator/DepthEstimator.py
@@ -5,6 +5,7 @@ import sys
 
 # Add vendor directory to path
 vendor_dir = os.path.abspath(os.path.join(os.path.dirname(__file__), '..', '..', '..', 'vendor', 'Depth-Anything-V2'))
+model_dir = os.path.abspath(os.path.join(os.path.dirname(__file__), '..', '..', '..', 'models'))
 print(vendor_dir)
 sys.path.append(vendor_dir)
 
@@ -19,10 +20,21 @@ class DepthEstimator:
     def __init__(self):
         """Initialize the DepthEstimator."""
         self.model = DepthAnythingV2(encoder='vits', features=64, out_channels=[48, 96, 192, 384])
-        self.model.load_state_dict(torch.load('../../../models/depth_anything_v2_vits.pth', map_location='cpu'))
+        device = 'cuda'
+        if not torch.cuda.is_available():
+            device = 'cpu'
+            print('WARNING: CUDA is not available, using CPU instead.')
+        
+# Disable memory efficient attention to avoid xFormers issues
+        torch.backends.cuda.enable_mem_efficient_sdp = False
+
+        self.model.load_state_dict(torch.load(os.path.join(model_dir, 'depth_anything_v2_vits.pth'), map_location=device, weights_only=True))
+        self.model.to(device)
         self.model.eval()
         
     def predict(self, image):
         """Predict depth from input."""
-        depth = self.model.infer_image(image) # HxW raw depth map
+        depth = self.model.infer_image(image)
+        # normalize
+        depth = (depth - depth.min()) / (depth.max() - depth.min())
         return depth
