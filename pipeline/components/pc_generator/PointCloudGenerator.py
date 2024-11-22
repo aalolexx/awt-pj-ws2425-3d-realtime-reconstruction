71895d059dfeec667c2a174b666bf3738a53354a
Alex Ehrenhöfer <ehren.alex@web.de>
2024-11-22 15:58:15 +0100

    create point cloud out of webcam frame

--- pipeline/components/pc_generator/PointCloudGenerator.py
+++ pipeline/components/pc_generator/PointCloudGenerator.py
@@ -13,6 +13,10 @@ class PointCloudGenerator:
         self.depth_estimator = DepthEstimator()
         self.cap = cv2.VideoCapture(0)
 
+        self.vis = o3d.visualization.Visualizer()
+        self.vis.create_window(width=800, height=600)
+        self.is_point_cloud_created = False
+
         if not self.cap.isOpened():
             raise Exception("Could not open webcam")
 
@@ -29,6 +33,8 @@ class PointCloudGenerator:
                 print("Failed to grab frame")
                 break
                 
+            frame = cv2.cvtColor(frame, cv2.COLOR_BGR2RGB)
+
             self.process_frame(frame)
             #end_time = time.time()
             #print(f"Depth prediction took: {end_time - start_time} seconds")
@@ -39,6 +45,7 @@ class PointCloudGenerator:
         # Release the capture when done
         self.cap.release()
         cv2.destroyAllWindows()
+        self.vis.destroy_window()
 
 
     def create_point_cloud(self, image_rgb, image_depth):
@@ -58,7 +65,7 @@ class PointCloudGenerator:
 
         # Stack and filter valid points
         points = np.dstack((x, y, z)).reshape(-1, 3)
-        valid_points = points[depth.reshape(-1) > 0]  # Remove zero-depth points
+        valid_points = points[depth.reshape(-1) > 0.3]  # Remove zero-depth points
 
         # Create an Open3D point cloud from the resulting 3D points
         pcd = o3d.geometry.PointCloud()
@@ -66,8 +73,9 @@ class PointCloudGenerator:
 
         # Set the colors from the RGB image
         rgb = np.asarray(image_rgb).reshape(-1, 3) / 255.0  # Normalize to [0, 1]
-        pcd.colors = o3d.utility.Vector3dVector(rgb[depth.reshape(-1) > 0])
+        pcd.colors = o3d.utility.Vector3dVector(rgb[depth.reshape(-1) > 0.3])
 
+        return pcd
         # Flip the point cloud (optional, depending on the coordinate system)
         #pcd.transform([[-1, 0, 0, 0],
         #            [0, -1, 0, 0],
@@ -84,11 +92,23 @@ class PointCloudGenerator:
         """
 
         depth = self.depth_estimator.predict(frame)
-        self.create_point_cloud(frame, depth)
-        print("frame")
+        pcd = self.create_point_cloud(frame, depth)
 
+        if not self.is_point_cloud_created:
+            self.vis.add_geometry(pcd)
+            self.is_point_cloud_created = True
+            self.pcd_placeholder = pcd 
+        else:
+            # Update points and colors of the existing point cloud
+            self.pcd_placeholder.points = pcd.points
+            self.pcd_placeholder.colors = pcd.colors
 
         # Display the frame
+        self.vis.update_geometry(self.pcd_placeholder)
+        self.vis.poll_events()
+        self.vis.update_renderer()
+
+        cv2.imshow('Webcam', frame)
         cv2.imshow('Webcam', depth)
 
 
